f9568e52ae2c5427c3fba0218460a258a6ff52a2
Joey Muller <jmullercuber@gmail.com>
2021-10-08 11:53:13 -0600

    Using styled-components

--- trps-client/src/pages/Home/Home.tsx
+++ trps-client/src/pages/Home/Home.tsx
@@ -1,23 +1,43 @@
 import logo from "./logo.svg";
 import "./Home.css";
 
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  text-align: center;
+`;
+
+const StyledHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const StyledLink = styled.a`
+  color: #61dafb;
+`;
+
 const Home = () => (
-  <div className="Home">
-    <header className="Home-header">
+  <StyledContainer>
+    <StyledHeader>
       <img src={logo} className="Home-logo" alt="logo" />
       <p>
         Edit <code>src/pages/Home/Home.tsx</code> and save to reload.
       </p>
-      <a
-        className="Home-link"
+      <StyledLink
         href="https://reactjs.org"
         target="_blank"
         rel="noopener noreferrer"
       >
         Learn React
-      </a>
-    </header>
-  </div>
+      </StyledLink>
+    </StyledHeader>
+  </StyledContainer>
 );
 
 export default Home;
